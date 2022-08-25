03e9677699f930c2d14414a94e3f9e7b64800721
Oscar <oscarhdec01@gmail.com>
2022-08-25 12:34:11 +0200

    Updated client area

--- app/assets/scripts/modules/ClientArea.js
+++ app/assets/scripts/modules/ClientArea.js
@@ -6,7 +6,7 @@ class ClientArea {
     this.form = document.querySelector(".client-area__form");
     this.field = document.querySelector(".client-area__input");
     this.contentArea = document.querySelector(".client-area__content-area");
-    this.events;
+    this.events();
   }
 
   events() {
@@ -18,7 +18,7 @@ class ClientArea {
 
   sendRequest() {
     Axios.post(
-      "https://cool-cactus-108f9b.netlify.app/.netlify/functions/secret-area",
+      "https://pensive-rosalind-383c21.netlify.com/.netlify/functions/secret-area",
       { password: this.field.value }
     )
       .then((response) => {
@@ -26,7 +26,7 @@ class ClientArea {
         this.contentArea.innerHTML = response.data;
       })
       .catch(() => {
-        this.contentArea.innerHTML = `<p class="client-area__error">The secret phrase is incorrect</p>`;
+        this.contentArea.innerHTML = `<p class="client-area__error">That secret phrase is not correct. Try again.</p>`;
         this.field.value = "";
         this.field.focus();
       });
@@ -36,16 +36,17 @@ class ClientArea {
     document.body.insertAdjacentHTML(
       "beforeend",
       `
-      <div class="client-area">
-  <div class="wrapper wrapper--medium">
-    <h2 class="section-title section-title--blue">Secret Client Area</h2>
-    <form class="client-area__form" action="">
-      <input class="client-area__input" type="text" placeholder="Enter the secret phrase">
-      <button class="btn btn--orange">Submit</button>
-    </form>
-    <div class="client-area__content-area"></div>
-  </div>
-</div>`
+    <div class="client-area">
+      <div class="wrapper wrapper--medium">
+        <h2 class="section-title section-title--blue">Secret Client Area</h2>
+        <form class="client-area__form" action="">
+          <input class="client-area__input" type="text" placeholder="Enter the secret phrase">
+          <button class="btn btn--orange">Submit</button>
+        </form>
+        <div class="client-area__content-area"></div>
+      </div>
+    </div>
+    `
     );
   }
 }
--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,8 @@
 exports.handler = function (event, context, callback) {
-  const secretContent = ` <h3>Welcolme to the secret area</h3>
- <p>Here  we can tell you that the sky is <strong> blue</strong> </p>`;
+  const secretContent = `
+  <h3>Welcome To The Secret Area</h3>
+  <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
+  `;
 
   let body;
 
