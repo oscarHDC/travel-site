203c58d2ad2425cbf570b1bbe8bf2058fbd89b91
Oscar <oscarhdec01@gmail.com>
2022-08-25 12:28:27 +0200

    Added clients area feture

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,9 +2,10 @@ import "../styles/styles.css";
 import "lazysizes";
 import MobileMenu from "./modules/MobileMenu.js";
 import RevealOnScroll from "./modules/RevealOnScroll";
-
 import StickyHeader from "./modules/StickyHeader";
+import ClientArea from "./modules/ClientArea";
 
+new ClientArea();
 new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
